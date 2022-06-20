b79e471cda99d6973c472a06328a2c837cfe77cd
Alberto Ventura <albertoventura.dev@gmail.com>
2022-06-20 17:54:52 -0300

    consult clean

--- dic/src/app/features/consult-dic/consult-dic.component.ts
+++ dic/src/app/features/consult-dic/consult-dic.component.ts
@@ -2,7 +2,7 @@ import { WordExtraComponent } from './../word-extra/word-extra.component';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Router } from '@angular/router';
-import { StorageService } from 'src/app/core';
+import { routerLabels, StorageService } from 'src/app/core';
 import { FilterWordsComponent } from 'src/app/shared';
 
 @Component({
@@ -11,12 +11,6 @@ import { FilterWordsComponent } from 'src/app/shared';
   styleUrls: ['./consult-dic.component.scss']
 })
 export class ConsultDicComponent implements OnInit {
-  wordList: any[] = [
-    {name: 'Aaaaaa', definition: 'Lorem ipsum dolor sit amet', extra: '' },
-    {name: 'Bbbb', definition: 'Sit amet dolor pog', extra: 'asd' },
-    {name: 'Cccc', definition: 'Sit amet dolor pog', extra: 'asd' },
-  ];
-
   dataRoute: any;
   baseArray: any[] = []
   wordArray: any[] = [];
@@ -27,7 +21,9 @@ export class ConsultDicComponent implements OnInit {
     private storage: StorageService,
   ) {
     this.dataRoute = this.router.getCurrentNavigation()?.extras.state;
-    console.log('dataRoute', this.dataRoute);
+    if(this.dataRoute == undefined){
+      this.router.navigate([routerLabels.listDic]);
+    }
     this.refreshArray();
   }
 
@@ -35,7 +31,6 @@ export class ConsultDicComponent implements OnInit {
   }
 
   openExtra(value?: any): void {
-    console.log(value);
     const dialogRef = this.dialog.open(
       WordExtraComponent,{
       data: {
@@ -48,30 +43,11 @@ export class ConsultDicComponent implements OnInit {
       },
       panelClass: 'dialog-word-extra'
     });
-
-    dialogRef.afterClosed().subscribe(result => {
-      if (result == undefined){
-        //this.user = this.backupUser;
-      }else{
-        console.log('volta do dialog', result);
-        //this.user = result;
-        //this.backupUser = result;
-        //this.updateUser(this.user);
-        //this.saveLocalStorage(this.user);
-      }
-    });
   }
   refreshArray(fromFilter?: boolean){
-    //console.log('sort',this.sortArray);
     this.baseArray = this.storage.listWords(this.dataRoute.id);
     this.sortAlphabeticaly();
     if(fromFilter){
-      /*
-      if(this.hasFilter){
-        this.filter.filterArray();
-        this.wordArray = this.filter.filteredArray;
-        return;
-      }*/
       this.wordArray = this.filter.filteredArray;
       return;
     }
